0c0bc68b02d4eab6ca9a8bd2dea018a8246e8263
Akhil Chauhan <akthehacker11@gmail.com>
2023-12-05 02:32:39 +0530

    update

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   const { dispatch: userPasteDispatch } = useUserPasteContext();
   const ref = useRef();
 
-  console.log(import.meta.env.VITE_BASE_URL);
+  console.log(`${import.meta.env.VITE_BASE_URL}/api/pastes/`);
 
   var config = {};
   if (user) {
